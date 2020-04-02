f071d4c39b8eb789fabd02b6753c9339ab1e1a63
0xJeremy <jeremy.kanovsky@tufts.edu>
2020-04-01 22:11:08 -0400

    Updated file structure

---
+++ python/ctrlengine/filters/__init__.py
@@ -0,0 +1,5 @@
+from .simple_moving_average import simple_moving_average
+simple_moving_average = simple_moving_average
+
+from .exponentially_weighted_moving_average import exponentially_weighted_moving_average
+exponentially_weighted_moving_average = exponentially_weighted_moving_average
