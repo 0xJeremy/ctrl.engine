c78fd7dd064b4ca1a92b5ba72dde960e4d3edffb
0xJeremy <jeremy.kanovsky@tufts.edu>
2020-05-09 19:46:13 -0400

    Added haar cascade class

--- ctrlengine/ai/test.py
+++ ctrlengine/ai/test.py
@@ -1,43 +1,73 @@
+###############################
+### TESTING USB ACCELERATOR ###
+###############################
+
 # import cv2
-# from pose_detection import pose_detection
 # from face_detection import face_detection
-# from image_segmentation import image_segmentation
-
-# engine = image_segmentation()
-# cam = cv2.VideoCapture(2)
-
 # import time
 
-# ret, frame = cam.read()
-# # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-# # engine.segment(frame)
-# # engine.write_image_to_file('test.jpg')
-# # img = engine.get_segmented_image()
+# engine = face_detection()
+# cam = cv2.VideoCapture(2)
 
 # while True:
+# 	start = time.time()
 # 	ret, frame = cam.read()
-# 	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
-# 	key = cv2.waitKey(10) & 0xFF
+# 	key = cv2.waitKey(10)
 # 	if key == ord('q'):
 # 		cam.release()
 # 		break
 # 	start = time.time()
-# 	engine.segment(frame)
+# 	engine.detect(frame)
+# 	boxes = engine.get_bounding_boxes()
+# 	for box in boxes:
+# 		cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255,0,0), 2)
+# 	cv2.imshow("frame", frame)
 # 	print("{:.2f} ms".format((time.time()-start)*1000))
-# 	# boxes = engine.get_bounding_boxes()
-# 	# for box in boxes:
-# 	# 	cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255,0,0), 2)
-# 	cv2.imshow("frame", engine.get_segmented_image())
 
-from cloud_vision import cloud_vision
-import cv2
-import time
+###############################################################
 
-engine = cloud_vision()
-cam = cv2.VideoCapture(2)
+############################
+### TESTING GOOGLE CLOUD ###
+############################
 
-ret, frame = cam.read()
+# from cloud_vision import cloud_vision
+# import cv2
+# import time
 
-start = time.time()
-print(engine.detect_faces(frame))
-print("{:.2f} ms".format((time.time()-start)*1000))
+# engine = cloud_vision()
+# cam = cv2.VideoCapture(2)
+
+# ret, frame = cam.read()
+
+# start = time.time()
+# print(engine.detect_faces(frame))
+# print("{:.2f} ms".format((time.time()-start)*1000))
+
+###############################################################
+
+############################
+### TESTING HAAR CASCADE ###
+############################
+
+# import cv2
+# from haar_cascade import haar_cascade
+# import time
+
+# engine = haar_cascade(model=haar_cascade.FACE)
+
+# cam = cv2.VideoCapture(2)
+
+# while True:
+# 	start = time.time()
+# 	ret, frame = cam.read()
+# 	faces = engine.detect(frame)
+# 	k = cv2.waitKey(10)
+# 	if k == ord('q'):
+# 		cam.release()
+# 		break
+# 	for (x,y,w,h) in faces:
+# 		cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+# 	cv2.imshow('frame', frame)
+# 	print("{:.2f} ms".format((time.time()-start)*1000))
+
+###############################################################
